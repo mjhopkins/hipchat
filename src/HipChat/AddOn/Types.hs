07d41a5cd63aae65669f7a72f75835b3759889c7
Mark Hopkins <markjohnhopkins@gmail.com>
2017-07-19 18:18:58 +1000

    HipChat.AddOn.Types

---
+++ src/HipChat/AddOn/Types.hs
@@ -0,0 +1,23 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module: HipChat.AddOn.Types
+-- Description: Common types used in the Add-on API.
+--
+--------------------------------------------------------------------------------
+
+module HipChat.AddOn.Types
+  ( RoomEvent(..)
+  , Registration(..)
+  , module HipChat.AddOn.Types.RoomEvent
+  , AuthenticationMethod(..)
+  , Icon(..)
+  , url
+  , url2x
+  , Name(..)
+  ) where
+
+import HipChat.AddOn.Registration
+import HipChat.AddOn.Types.RoomEvent
+import HipChat.AddOn.Types.AuthenticationMethod
+import HipChat.AddOn.Types.Icon
+import HipChat.AddOn.Types.Name
